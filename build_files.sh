f4df82ced5f719d14d3d79bfc5d2518034094316
DonLangosta <>
2023-05-26 23:54:37 +0200

    Pls Work

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
+
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+
+python manage.py collectstatic --noinput --clear
